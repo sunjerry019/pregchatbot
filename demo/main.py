165f604e4c7606c12f6cce586a74e2368419e656
Sun Yudong <sunjerry019@gmail.com>
2019-05-14 09:42:52 +0800

    add a question to main for debugging

--- demo/main.py
+++ demo/main.py
@@ -2,6 +2,8 @@ import engine
 
 chatbot = engine.Engine()
 
+print(chatbot.ask("Can I eat raw food during pregnancy?"))
+
 
 #chatbot.ask(s) #s is user input string
 #chatbot.add_to_questionbank(m) # m is user input string
